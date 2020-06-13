6dfe15f29d304f5f6abf07413813c4ce94c7f828
Andrew Rozhkov <buhhater@gmail.com>
2020-06-13 13:32:12 +0300

    working with Picasso

--- app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/telegram/ui/fragments/SettingsFragment.kt
@@ -1,14 +1,17 @@
 package com.example.telegram.ui.fragments
 
+import android.app.Activity
 import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.fragment.app.Fragment
+import com.example.telegram.MainActivity
 import com.example.telegram.R
 import com.example.telegram.activities.RegisterActivity
 import com.example.telegram.utilits.*
+import com.squareup.picasso.Picasso
 import com.theartofdev.edmodo.cropper.CropImage
 import com.theartofdev.edmodo.cropper.CropImageView
 import kotlinx.android.synthetic.main.fragment_settings.*
@@ -40,7 +43,7 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
             .setAspectRatio(1,1)
             .setRequestedSize(600, 600)
             .setCropShape(CropImageView.CropShape.OVAL)
-            .start(APP_ACTIVITY)
+            .start(APP_ACTIVITY, this)
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
@@ -56,5 +59,34 @@ class SettingsFragment : BaseFragment(R.layout.fragment_settings) {
             R.id.settings_menu_change_name -> replaceFragment(ChangeNameFragment())
         }
         return true
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode== CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE
+            && resultCode == RESULT_OK && data != null){
+            val uri = CropImage.getActivityResult(data).uri
+            val path = REF_STORAGE_ROOT.child(FOLDER_PROFILE_IMAGE)
+                .child(CURRENT_UID)
+            path.putFile(uri).addOnCompleteListener{ task1 ->
+                if (task1.isSuccessful) {
+                    path.downloadUrl.addOnCompleteListener { task2 ->
+                        if (task2.isSuccessful){
+                            val photoUrl = task2.result.toString()
+                            REF_DATABASE_ROOT.child(NODE_USERS).child(CURRENT_UID)
+                                .child(CHILD_PHOTO_URL).setValue(photoUrl)
+                                .addOnCompleteListener {
+                                    if (it.isSuccessful){
+                                        settings_user_photo.downloadAndSetImage(photoUrl)
+                                        showToast(getString(R.string.toast_data_update))
+                                        USER.photoUrl = photoUrl
+                                    }
+                                }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
--- app/src/main/java/com/example/telegram/utilits/funs.kt
+++ app/src/main/java/com/example/telegram/utilits/funs.kt
@@ -1,12 +1,17 @@
 package com.example.telegram.utilits
 
+import android.content.Context
 import android.content.Intent
+import android.view.inputmethod.InputMethodManager
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.example.telegram.R
 import com.example.telegram.activities.RegisterActivity
 import com.example.telegram.ui.fragments.ChatsFragment
+import com.squareup.picasso.Picasso
+import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.fragment_settings.*
 
 fun showToast(message: String){
     Toast.makeText(APP_ACTIVITY, message, Toast.LENGTH_SHORT).show()
@@ -36,4 +41,16 @@ fun Fragment.replaceFragment(fragment: Fragment){
         ?.addToBackStack(null)
         ?.replace(R.id.dataContainer,fragment)
         ?.commit()
+}
+
+fun hideKeyboard(){
+    val imm: InputMethodManager = APP_ACTIVITY.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(APP_ACTIVITY.window.decorView.windowToken, 0)
+}
+
+fun CircleImageView.downloadAndSetImage(url: String){
+    Picasso.get()
+        .load(url)
+        .placeholder(R.drawable.ic_default_user)
+        .into(this)
 }
