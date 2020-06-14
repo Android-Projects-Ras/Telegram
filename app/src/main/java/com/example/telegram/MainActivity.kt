6078475bdd29a8daedd3a153ec0c0cb2c7eb0b6d
Andrew Rozhkov <buhhater@gmail.com>
2020-06-14 13:18:04 +0300

    setting up user state: online, offline, typing

--- app/src/main/java/com/example/telegram/MainActivity.kt
+++ app/src/main/java/com/example/telegram/MainActivity.kt
@@ -49,4 +49,14 @@ class MainActivity : AppCompatActivity() {
         mAppDrawer = AppDrawer(this, mToolbar)
     }
 
+    override fun onStart() {
+        super.onStart()
+        AppStates.updateState(AppStates.ONLINE)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        AppStates.updateState(AppStates.OFFLINE)
+    }
+
 }
